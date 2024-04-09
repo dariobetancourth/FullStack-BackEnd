82c7e2cbcf11480c177dedf49801ade8c3c66c95
dariobetancourth <156965416+dariobetancourth@users.noreply.github.com>
2024-04-09 08:11:40 -0600

    posting

--- api/cs415/cs415/settings.py
+++ api/cs415/cs415/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     
 ]
 
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',  # Add CorsMiddleware here
 
     
 ]
@@ -136,4 +138,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CORS_ORIGIN_ALLOW_ALL = True  # Allow all origins
 
--- api/cs415/cs415/urls.py
+++ api/cs415/cs415/urls.py
@@ -2,7 +2,7 @@
 URL configuration for cs415 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,7 +16,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from cs415 import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', views.Login.as_view()),
+    path('users/', views.UserAPIView.as_view()),
+    path('addresses/', views.AddressAPIView.as_view()),
+    path('phones/', views.PhoneAPIView.as_view()),
+    path('phone-types/', views.PhoneTypeAPIView.as_view()),
+    path('address-types/', views.AddressTypeAPIView.as_view()),
+    path('user-infos/', views.UserInfoAPIView.as_view()),
+    path('pages/', views.PageDataAPIView.as_view()),
+    path('users/user/<int:id>', views.GetSingleUserAPIView.as_view()),
+    path('addresses/user/<int:id>', views.UserAddressAPIView.as_view()),
+    path('phones/user/<int:id>', views.UserPhoneAPIView.as_view()),
+    path('user-infos/user/<int:id>', views.GetSingleUserInfoAPIView.as_view()),
+    path('pages/page/<int:id>', views.GetSinglePageDataAPIView.as_view()),
 ]
